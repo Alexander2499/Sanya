daf057942d1d61d33723ef5bfaa4fec4ce073032
Саня <alecs.korotkow@mail.ru>
2023-09-05 22:36:30 +0300

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
         System.out.println(plus(f));
 
 
-        System.out.println("test123");
+        System.out.println("rrrrrrrr");
         System.out.println("test");
     }
 }
